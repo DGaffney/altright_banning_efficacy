8c558e661bc1e39088ce3506e1e564aed5c6670c
Devin Gaffney <itsme@devingaffney.com>
2017-06-22 14:00:48 -0400

    updates

--- baumgartner_data/commands/extract_altrighter_timelines.rb
+++ baumgartner_data/commands/extract_altrighter_timelines.rb
@@ -7,46 +7,33 @@ class ExtractAltrightContent
   sidekiq_options queue: :extract_altright_content
   def perform(file)
     if file.include?("RS_")
-      csv = CSV.open("#{FILEPATH}/baumgartner_data/submissions_altrighters/#{file}", "w")
-      CSV.foreach("#{FILEPATH}/baumgartner_data//#{file}", col_sep: ",", row_sep: "\n") do |row|
-        csv << row if ALTRIGHT_USERS.include?(row[2])
-      end;false
-      csv.close
+      ALTRIGHT_USERS.each_slice(100) do |user_slice|
+        `bzip2 -dck #{FILEPATH}/baumgartner_data/submissions_raw/#{file} | jq -r 'select([.author] | inside([\"#{user_slice.join('", "')}\"])) | [(.created_utc | tostring), .subreddit, .author, .id, .selftext, .title] | @csv' >> #{FILEPATH}/baumgartner_data/submissions_altrighters/#{file.split(".").first}.csv`
+      end
     elsif file.include?("RC_")
-      csv = CSV.open("#{FILEPATH}/baumgartner_data/comments_altrighters/#{file}", "w")
-      CSV.foreach("/media/dgaff/Main/wehrwein_altright_content/comments/#{file}", col_sep: ",", row_sep: "\n") do |row|
-        csv << row if ALTRIGHT_USERS.include?(row[2])
-      end;false
-      csv.close
+      ALTRIGHT_USERS.each_slice(100) do |user_slice|
+        `bzip2 -dck #{FILEPATH}/baumgartner_data/comments_raw/#{file} | jq -r 'select([.author] | inside([\"#{user_slice.join('", "')}\"])) | [(.created_utc | tostring), .subreddit, .author, .id, .parent_id, .body] | @csv' >> #{FILEPATH}/baumgartner_data/comments_altrighters/#{file.split(".").first}.csv`
+      end
     end
   end
 
   def self.parse_missing_files
-    csv = CSV.open("/media/dgaff/Main/wehrwein_altright_content/comments_altrighters/missing.csv", "w")
-    CSV.foreach("/media/dgaff/Main/wehrwein_altright_content/comments/missing.csv") do |row|
-      csv << row if ALTRIGHT_USERS.include?(row[2])
-    end;false
-    csv.close
-    csv = CSV.open("/media/dgaff/Main/wehrwein_altright_content/submissions_altrighters/missing.csv", "w")
-    CSV.foreach("/media/dgaff/Main/wehrwein_altright_content/submissions/missing.csv") do |row|
-      csv << row if ALTRIGHT_USERS.include?(row[2])
-    end;false
-    csv.close
+    ALTRIGHT_USERS.each_slice(100) do |user_slice|
+      `bzip2 -dck #{FILEPATH}/baumgartner_data/submissions_raw/missing_subreddits_0-10m.json.bz2 | jq -r 'select([.author] | inside([\"#{user_slice.join('", "')}\"])) | [(.created_utc | tostring), .subreddit, .author, .id, .selftext, .title] | @csv' >> #{FILEPATH}/baumgartner_data/submissions_altrighters/missing_subreddits_0-10m.csv`
+      `bzip2 -dck #{FILEPATH}/baumgartner_data/comments_raw/missing_comments.json.bz2 | jq -r 'select([.author] | inside([\"#{user_slice.join('", "')}\"])) | [(.created_utc | tostring), .subreddit, .author, .id, .selftext, .title] | @csv' >> #{FILEPATH}/baumgartner_data/comments_altrighters/missing_comments.csv`
+    end
   end
   
   def self.kickoff_parallel
-  #note that this requires running Sidekiq: in the commands folder, run `sidekiq -r ./generate_altright_timelines.rb -q extract_altright_content -c 50` to run 50 instances in parallel. redis-server must also be running on the machine in order for this to work. For additional information about sidekiq, please consult https://github.com/mperham/sidekiq.
-    `ls /media/dgaff/Main/wehrwein_altright_content/submissions`.split("\n").select{|x| x.include?("RS_")}.collect{|x| ExtractAltrightContent.perform_async(x)}
-    `ls /media/dgaff/Main/wehrwein_altright_content/comments`.split("\n").select{|x| x.include?("RC_")}.collect{|x| ExtractAltrightContent.perform_async(x)}
+    #note that this requires running Sidekiq: in the commands folder, run `sidekiq -r ./generate_altright_timelines.rb -q extract_altright_content -c 50` to run 50 instances in parallel. redis-server must also be running on the machine in order for this to work. For additional information about sidekiq, please consult https://github.com/mperham/sidekiq.
+    `ls #{FILEPATH}/baumgartner_data/submissions_raw`.split("\n").select{|f| f.include?("RS_")}.collect{|x| ExtractAltrightContent.perform_async(x)}
+    `ls #{FILEPATH}/baumgartner_data/comments_raw`.split("\n").select{|f| f.include?("RC_")}.collect{|x| ExtractAltrightContent.perform_async(x)}
     self.parse_missing_files
   end
   
   def self.kickoff_sequential
-    `ls /media/dgaff/Main/wehrwein_altright_content/submissions`.split("\n").select{|x| x.include?("RS_")}.collect{|x| ExtractAltrightContent.perform_async(x)}
-    `ls /media/dgaff/Main/wehrwein_altright_content/comments`.split("\n").select{|x| x.include?("RC_")}.collect{|x| ExtractAltrightContent.perform_async(x)}
+    `ls #{FILEPATH}/baumgartner_data/submissions_raw`.split("\n").select{|f| f.include?("RS_")}.collect{|x| ExtractAltrightContent.new.perform(x)}
+    `ls #{FILEPATH}/baumgartner_data/comments_raw`.split("\n").select{|f| f.include?("RC_")}.collect{|x| ExtractAltrightContent.new.perform(x)}
     self.parse_missing_files
   end
 end
-"bzip2 -dck $(pwd)/baumgartner_data/submissions_raw/RS_2016-09.bz2 | jq  -r 'select([.author] | inside(["aquma", "MisterBreeze", "The_New_Ent"]))'"
-"bzip2 -dck $(pwd)/baumgartner_data/submissions_raw/RS_2016-09.bz2 | jq  -r ' select(.author inside([\"#{["aquma", "MisterBreeze", "The_New_Ent"].join('", "')}\"]))'"
-'select(.items | index("blue"))'
