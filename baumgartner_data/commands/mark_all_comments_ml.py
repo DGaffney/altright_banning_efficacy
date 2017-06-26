6602469ec6cc1da464ef9915703552c791b88c5c
Devin Gaffney <itsme@devingaffney.com>
2017-06-25 20:21:33 -0400

    updates

--- baumgartner_data/commands/mark_all_comments_ml.py
+++ baumgartner_data/commands/mark_all_comments_ml.py
@@ -1,3 +1,4 @@
+import json
 import pickle
 import csv
 import argparse
@@ -7,3 +8,16 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", help="file to learn from")
 args = vars(ap.parse_args())
 model = pickle.loads(open("ml_model_altright.pkl").read())
+def read_csv(filename):
+  dataset = []
+  i = 0
+  with open(filename, 'rb') as f:
+      reader = csv.reader(f)
+      for row in reader:
+        if i != 0:  
+          dataset.append([float(el) for el in row])
+        i += 1
+  return dataset
+
+dataset = read_csv(args['file'])
+print json.dumps(model.fit(dataset))
