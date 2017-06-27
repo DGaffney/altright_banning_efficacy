b732c463761ff727cf37b6f7b317f996e1e65e3a
Devin Gaffney <itsme@devingaffney.com>
2017-06-26 22:12:46 -0400

    updates

--- baumgartner_data/commands/mark_all_comments_ml.py
+++ baumgartner_data/commands/mark_all_comments_ml.py
@@ -32,23 +32,23 @@ def read_csv_str(filename):
 
 raw_dataset = read_csv_str(filepath+"/baumgartner_data/comments_altrighters/"+args['file'])
 keyword_groups = [set(el) for el in json.loads(open(filepath+"/baumgartner_data/machine_learning/keyword_groups.json").read())]
-csv = []
+csv_data = []
 ii = 0
 for row in raw_dataset:
   parsed = set(re.split(ur"[\u200b\s]+", re.sub('[%s]' % re.escape(string.punctuation), ' ', row[-1].lower()), flags=re.UNICODE))
   new_row = []
   for kg in keyword_groups:
     new_row.append(len(kg.intersection(parsed)))
-  csv.append(new_row)
+  csv_data.append(new_row)
   ii += 1
   if ii % 1000 == 0:
     print ii
 
 with open(filepath+"/baumgartner_data/comments_altrighters_ml_transformed/"+args['file'], 'wb') as f:
   writer = csv.writer(f)
-  writer.writerows(csv)
+  writer.writerows(csv_data)
 
-predictions = model.predict(csv).tolist()
+predictions = model.predict(csv_data).tolist()
 raw_merged = []
 for i,row in enumerate(raw_dataset):
   row.append(predictions[i])
