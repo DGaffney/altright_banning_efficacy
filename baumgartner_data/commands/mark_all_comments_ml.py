d3dfe87124c05d54a1d02935eec509d4a3b5a88a
Devin Gaffney <itsme@devingaffney.com>
2017-06-26 22:02:27 -0400

    updates

--- baumgartner_data/commands/mark_all_comments_ml.py
+++ baumgartner_data/commands/mark_all_comments_ml.py
@@ -8,7 +8,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", help="file to learn from")
 ap.add_argument("-m", "--model", help="model to be used")
 args = vars(ap.parse_args())
-model = pickle.loads(open(args['model']).read())
+model = pickle.loads(open(filepath+"/baumgartner_data/commands/ml_model_altright.pkl").read())
 def read_csv(filename):
   dataset = []
   i = 0
@@ -18,5 +18,40 @@ def read_csv(filename):
         dataset.append([float(el) for el in row])
   return dataset
 
-dataset = read_csv(args['file'])
-print json.dumps(model.predict(dataset).tolist())
+def read_csv_str(filename):
+  dataset = []
+  i = 0
+  with open(filename, 'rb') as f:
+      reader = csv.reader(f)
+      for row in reader:
+        dataset.append(row)
+  return dataset
+
+filepath = os.popen('git rev-parse --show-toplevel').read().strip()
+raw_dataset = read_csv_str(filepath+"/baumgartner_data/comments_altrighters/"+file)
+keyword_groups = [set(el) for el in json.loads(open(filepath+"/baumgartner_data/machine_learning/keyword_groups.json").read())]
+csv = []
+ii = 0
+for row in raw_dataset:
+  parsed = set(re.split(ur"[\u200b\s]+", re.sub('[%s]' % re.escape(string.punctuation), ' ', row[-1].lower()), flags=re.UNICODE))
+  new_row = []
+  for kg in keyword_groups:
+    new_row.append(len(kg.intersection(parsed)))
+  csv.append(new_row)
+  ii += 1
+  if ii % 1000 == 0:
+    print ii
+
+with open(filepath+"/baumgartner_data/comments_altrighters_ml_transformed/"+file, 'wb') as f:
+  writer = csv.writer(f)
+  writer.writerows(csv)
+
+predictions = model.predict(csv).tolist()
+raw_merged = []
+for i,row in enumerate(raw_dataset):
+  row.append(predictions[i])
+  raw_merged.append(row)
+
+with open(filepath+"/baumgartner_data/comments_altrighters_ml_predicted/"+file, 'wb') as f:
+  writer = csv.writer(f)
+  writer.writerows(raw_merged)
--- baumgartner_data/commands/transform_altright_content.rb
+++ baumgartner_data/commands/transform_altright_content.rb
@@ -8,24 +8,27 @@ class TransformAltrightContent
   include Sidekiq::Worker
   sidekiq_options queue: :train_ml_altright
   def perform(file)
-    raw_dataset = CSV.read("#{FILEPATH}/baumgartner_data/comments_altrighters/#{file}", row_sep: "\n", col_sep: ",");false
-    return nil if raw_dataset.count == 0
-    keyword_groups = JSON.parse(File.read("#{FILEPATH}/baumgartner_data/machine_learning/keyword_groups.json"));false
-    csv = CSV.open("#{FILEPATH}/baumgartner_data/comments_altrighters_ml_transformed/#{file}", "w")
-    ii = 0
-    raw_dataset.each do |row|
-      parsed = row.last.downcase.split(/[^[[:word:]]]+/)
-      csv << keyword_groups.collect{|kg| (kg & parsed).length}
-      ii += 1
-      puts ii if ii % 1000 == 0
-    end
-    csv.close
-    predicted = JSON.parse(`python #{FILEPATH}/baumgartner_data/commands/mark_all_comments_ml.py -m #{FILEPATH}/baumgartner_data/commands/ml_model_altright.pkl -f #{FILEPATH}/baumgartner_data/comments_altrighters_ml_transformed/#{file}`)
-    csv = CSV.open("#{FILEPATH}/baumgartner_data/comments_altrighters_ml_predicted/#{file}", "w")
-    raw_dataset.zip(predicted).each do |row|
-      csv << row.flatten
-    end
-    csv.close
+    `python #{FILEPATH}/baumgartner_data/commands/mark_all_comments_ml.py -f #{FILEPATH}/baumgartner_data/comments_altrighters_ml_transformed/#{file}`
+#    raw_dataset = CSV.read("#{FILEPATH}/baumgartner_data/comments_altrighters/#{file}", row_sep: "\n", col_sep: ",");false
+#    return nil if raw_dataset.count == 0
+#    keyword_groups = JSON.parse(File.read("#{FILEPATH}/baumgartner_data/machine_learning/keyword_groups.json"));false
+#    csv = CSV.open("#{FILEPATH}/baumgartner_data/comments_altrighters_ml_transformed/#{file}", "w")
+#    ii = 0
+#    raw_dataset.each do |row|
+#      parsed = row.last.downcase.split(/[^[[:word:]]]+/)
+#      csv << keyword_groups.collect{|kg| (kg & parsed).length}
+#      keyword_groups.collect{|kg| (kg & parsed).length}
+#      keyword_groups.collect{|kg| parsed.collect{|el| kg.index(el)}.compact.length}
+#      ii += 1
+#      puts ii if ii % 1000 == 0
+#    end
+#    csv.close
+#    predicted = JSON.parse(`python #{FILEPATH}/baumgartner_data/commands/mark_all_comments_ml.py -f #{FILEPATH}/baumgartner_data/comments_altrighters_ml_transformed/#{file}`)
+#    csv = CSV.open("#{FILEPATH}/baumgartner_data/comments_altrighters_ml_predicted/#{file}", "w")
+#    raw_dataset.zip(predicted).each do |row|
+#      csv << row.flatten
+#    end
+#    csv.close
   end
   
   def self.kickoff
