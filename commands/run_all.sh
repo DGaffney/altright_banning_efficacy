983c0808fdee4000e33de03885f1d07dda9ae47d
Devin Gaffney <itsme@devingaffney.com>
2017-07-09 15:19:06 -0400

    updates

--- commands/run_all.sh
+++ commands/run_all.sh
@@ -1,7 +1,7 @@
 cd $(git rev-parse --show-toplevel)
 echo $(pwd)
-./baumgartner_data/commands/mkdirs.sh
-./baumgartner_data/commands/download.sh
-./baumgartner_data/commands/extract_background.sh
-./baumgartner_data/commands/extract_altright_screen_names.sh
-ruby baumgartner_data/commands/extract_altright_content.rb
+./commands/mkdirs.sh
+./commands/download.sh
+./commands/extract_background.sh
+./commands/extract_altright_screen_names.sh
+ruby commands/extract_altright_content.rb
